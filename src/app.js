6194b9d05d95cbf119633cc768c9d55bee5eeee6
Javi Moreno <jmoreno.zn@gmail.com>
2015-03-22 13:39:39 +0100

    Búsqueda de paradas sin signos.

--- src/app.js
+++ src/app.js
@@ -320,6 +320,115 @@ var verAjustes = function() {
   ajustesCard.show();
 };
 
+// Pantalla de busqueda de paradas introduciendo el número de la misma
+
+var searchWindow = function () {
+  
+  var inputWindow = new UI.Window();
+
+  var numbers = [];
+  var stopId = [0, 0, 0, 0];
+  var begin = 7;
+  var numberSelected = 0;
+  
+  var title = new UI.Text({
+    position: new Vector2(0, 10),         
+    size: new Vector2(144, 168),
+    text: 'Parada',
+    font: 'GOTHIC_28_BOLD',
+    color: 'white',
+    textOverflow: 'wrap',
+    textAlign: 'center',
+    backgroundColor: 'black'
+  });
+  
+  inputWindow.add(title);
+
+  for (var i = 0; i < 4; i++) {
+    begin = 7 + (i * 33);
+    var number = new UI.Text({
+      position: new Vector2(begin, 60),
+      size: new Vector2(30, 40),
+      text: stopId[i],
+      font: 'bitham-30-black',
+      color: 'white',
+      textAlign: 'center',
+      borderColor: 'white',
+      backgroundColor: 'black'
+    });
+    numbers.push(number);
+  }
+
+  numbers[numberSelected].color('black');
+  numbers[numberSelected].backgroundColor('white');
+
+  for (i = 0; i < 4; i++) {
+    inputWindow.add(numbers[i]);
+  }
+
+  inputWindow.show();
+
+  inputWindow.on('click', 'up', function () {
+    if (stopId[numberSelected] === 9) {
+      stopId[numberSelected] = 0;
+    } else {
+      stopId[numberSelected] += 1;
+    }
+    numbers[numberSelected].text(stopId[numberSelected]);
+  });
+
+  inputWindow.on('click', 'down', function () {
+    if (stopId[numberSelected] === 0) {
+      stopId[numberSelected] = 9;
+    } else {
+      stopId[numberSelected] -= 1;
+    }
+    numbers[numberSelected].text(stopId[numberSelected]);
+  });
+
+  inputWindow.on('click', 'select', function() {
+    if (numberSelected === 3) {
+      var parada = stopId[0] * 1000 + stopId[1] * 100 + stopId[2] * 10 + stopId[3];
+      var checkCard = new UI.Card({
+        title: 'Próximas llegadas a parada:',
+        subtitle: parada,
+        action: {
+          up: 'images/action_icon_discard.png',
+          down: 'images/action_icon_check.png'
+        }
+      });
+            
+      checkCard.on('click', 'up', function(){
+        numbers[numberSelected].color('white');
+        numbers[numberSelected].backgroundColor('black');
+        numberSelected = 0;
+        numbers[numberSelected].color('black');
+        numbers[numberSelected].backgroundColor('white');    
+        checkCard.hide();
+      });
+            
+      checkCard.on('click', 'down', function(){
+        var paradaBuscar = {
+          stopId: parada,
+          postalAddress: ''
+        };
+        proximasLlegadas(paradaBuscar);
+        checkCard.hide();
+        inputWindow.hide();
+      });
+            
+      checkCard.show();
+  
+    } else {
+      numbers[numberSelected].color('white');
+      numbers[numberSelected].backgroundColor('black');
+      numberSelected += 1;
+      numbers[numberSelected].color('black');
+      numbers[numberSelected].backgroundColor('white');    
+    }
+  });
+  
+};
 
 // ESTA ES LA ZONA DONDE SE DEFINE E INSTANCIA EL MENÚ PRINCIPAL
 
@@ -332,6 +441,9 @@ var principalMenu = new UI.Menu({
     }, {
       title: 'Favoritas',
       icon: 'images/menu_icon_starred.png'
+    }, {
+      title: 'Buscar',
+      icon: 'images/menu_icon_search.png'
     }, {
       title: 'Ajustes',
       icon: 'images/menu_icon_settings.png'
@@ -350,6 +462,10 @@ principalMenu.on('select', function(e) {
       break;
 
     case 2:
+      searchWindow();
+      break;
+
+    case 3:
       verAjustes();
       break;
 
