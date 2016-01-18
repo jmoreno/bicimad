e3b68c52e9845be4d4d96453c64a37c081042d9c
jmoreno <jmoreno.zn@gmail.com>
2016-01-18 14:05:50 +0100

    Di algo

--- src/app.js
+++ src/app.js
@@ -439,6 +439,7 @@ var searchWindow = function () {
       var parada = stopId[0] * 1000 + stopId[1] * 100 + stopId[2] * 10 + stopId[3];
       var checkCard = new UI.Card({
       	fullscreen: true,
+      	color: 'white',
       	backgroundColor: 'black',
         title: 'Próximas llegadas a parada:',
         subtitle: parada,
